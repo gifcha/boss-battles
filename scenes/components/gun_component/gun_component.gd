3a334c051fbaf94e3bcc046da640d71a5d765948
Rudolfs Briedis <rudolfsbriedis77@gmail.com>
2023-12-04 21:32:16 +0200

    fixed bug with gun_component files

---
+++ scenes/components/gun_component/gun_component.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+
+@export_group("Required Nodes")
+@export var Sprite: Sprite2D
+@export var BarrelMarker: Marker2D
+
+@export_group("Gun Properties")
+@export var BulletSpeed: float ## Distance in units, the bullet travels in a second (1 unit = 100px) 
+@export var BulletDamage: int
+@export var ReloadTime: float ## Time it takes to reaload the gun (in seconds) 
+@export var AfterShotDelay: float ## Delay (in seconds) AFTER each shot
+@export var BeforeShotDelay: float ## Delay (in seconds) BEFORE each shot
+
+func spawnBullet():
+	return Node
+
+func canShoot():
+	return true
+
+func reload():
+	print("reloading")
+
+func shoot():
+	pass
