656f68d39bedb002801efc5d24f0ec371c07387c
Rudolfs Briedis <rudolfsbriedis77@gmail.com>
2023-12-16 01:34:30 +0200

    started on implementing screen shake

--- global_variables.gd
+++ global_variables.gd
@@ -4,6 +4,10 @@ class_name Global
 
 const UNIT_SIZE = 100 ## size of 1 unit in pixels
 
+
+static var player : Player
+
+
 static func getRootNode(node: Node): ## returns the root node (usually world) of the given node
 	var world = node.get_tree().current_scene
 	return world
@@ -12,3 +16,4 @@ static func spawnScene(scene: PackedScene, parentNode: Node): ## spawns a node i
 	var node = scene.instantiate()
 	parentNode.add_child(node)
 	return node
+
