6b6d47f6a5ade09aa0a5967f6f06a38a38a32de2
Rudolfs <rudolfsbriedis77@gmail.com>
2024-05-31 17:37:10 +0300

    started work on boss logic

--- scenes/components/enemy/enemy.gd
+++ scenes/components/enemy/enemy.gd
@@ -4,6 +4,14 @@ class_name Enemy
 @export var speed : int = 100
 @export var health : int = 100
 
+@export_group("scripts")
+@export var actionManager : GDScript
+@export var Attack : GDScript
+@export var Move : GDScript
+@export var Ability1 : GDScript
+@export var Ability2 : GDScript
+@export var Ability3 : GDScript
+
 @onready var movement = $movement
 var ticker : int = 0
 const physicsTick = 60
@@ -13,9 +21,8 @@ func take_damage(damage):
 	self.health -= damage
 	print(damage)
 
-
 func _ready():
-	pass
+	set_script(actionManager)
 
 
 func _process(delta):
