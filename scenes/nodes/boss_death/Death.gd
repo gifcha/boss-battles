100f2335614951b22201026d146bd8e838a68f3e
Rudolfs <rudolfsbriedis77@gmail.com>
2024-06-06 20:21:58 +0300

    deleted movement.gd and started fixing animations

--- scenes/nodes/boss_death/Death.gd
+++ scenes/nodes/boss_death/Death.gd
@@ -6,24 +6,28 @@ extends Enemy
 
 func _ready():
 	super()
+	animation.play("idle")
 
-var lastState = 0
+
+func tryToPlayAnim(name : String):
+	if animation.current_animation == "idle":
+		animation.play(name)
+	else:
+		animation.queue(name)
+
 func _process(delta):
 	super(delta) # izsauc process metodi no Enemy klases
 	$HealthBar.value = health
 	
-	if lastState != AM.currentState:
-		animation.play("RESET")
 	
 	# get the state from actionManager and execute task depending on that state
 	if AM.currentState == AM.state.idle:
-		animation.play("idle")
+		tryToPlayAnim("idle")
 	elif AM.currentState == AM.state.move:
-		animation.play("idle")
+		tryToPlayAnim("idle")
 		$movement.path = Navigation.request_path(global_position, Global.player.global_position)
 	elif AM.currentState == AM.state.attack or AM.currentState == AM.state.ability2 or AM.currentState == AM.state.ability3:
-		animation.play("attack")
+		tryToPlayAnim("attack")
 	elif AM.currentState == AM.state.ability1:
-		animation.play("teleport")
-	
-	lastState = AM.currentState
+		tryToPlayAnim("teleport")
+	#await animation.animation_finished
